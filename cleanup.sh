ab5724c0f7994cfcbb5d5e243a2bf86e2605ab56
Tim Jackleus <timjackleus@gmail.com>
2024-01-31 11:58:12 +0100

    docs: update readme (#11)

---
+++ cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker compose stop &&
+docker compose down -v &&
+docker volume rm -f ledyer-checkout-for-magento2_mgdata &&
+docker volume rm -f ledyer-checkout-for-magento2_dbdata &&
+docker volume rm -f ledyer-checkout-for-magento2_esdata &&
+docker compose rm
